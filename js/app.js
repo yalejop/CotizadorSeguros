07d2040685da7ec4d0508ed531afe167907d4b91
yalejop <alejo.9211@hotmail.com>
2022-02-05 23:57:18 -0500

    create the prototypes for project and the first function in UI prototype

---
+++ js/app.js
@@ -0,0 +1,31 @@
+//contructores
+function Seguro(marca, year, tipo){
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo
+}
+
+function UI() {}
+
+//llenar las opciones de los años
+
+UI.prototype.llenarOpciones = () => {
+    const max = new Date().getFullYear(),
+          min = max - 20;
+
+    const selectYear = document.querySelector('#year');
+
+    for (let i = max; i > min; i--) {
+        let option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        selectYear.appendChild(option);
+    }
+}
+
+//instanciar UI
+const ui = new UI();
+
+document.addEventListener('DOMContentLoaded', () => {
+    ui.llenarOpciones(); //llena el select con los años...
+})
